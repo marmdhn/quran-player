530a82987636d4e1afe028b846bae1c871b21d90
marmdhn <makbarramadhan62@gmail.com>
2025-11-06 22:19:24 +0700

    feat: add constant config

---
+++ lib/core/config/constant.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  static const double kDefaultPadding = 16.0;
+  static const double kDefaultRadius = 12.0;
+  static const int kSplashDelay = 5;
+
+  static const String keyUserToken = "user_token";
+  static const String keyOnboardingShown = "onboarding_shown";
+}
