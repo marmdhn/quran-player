85155d5b0700771b125ca119b3827ac86dd5ee00
marmdhn <makbarramadhan62@gmail.com>
2025-11-08 01:42:03 +0700

    feat: integrate SurahViewModel into SurahListScreen with dynamic Surah list loading and UI enhancements

--- lib/presentation/screens/surah-list/surah_list_screen.dart
+++ lib/presentation/screens/surah-list/surah_list_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:quran_player/core/config/constant.dart';
+import 'package:quran_player/presentation/shared/widgets/loading_indicator.dart';
 
-import '../../view-models/quran_player_view_model.dart';
+import '../../../data/models/surah_model.dart';
+import '../../view-models/reciter_view_model.dart';
+import '../../view-models/surah_view_model.dart';
 
 class SurahListScreen extends StatefulWidget {
   const SurahListScreen({super.key});
@@ -11,35 +15,98 @@ class SurahListScreen extends StatefulWidget {
 }
 
 class _SurahListScreenState extends State<SurahListScreen> {
-  final List<Map<String, dynamic>> surahList = [
-    {"number": 1, "englishName": "Al-Fatihah", "name": "الفاتحة"},
-    {"number": 2, "englishName": "Al-Baqarah", "name": "البقرة"},
-    {"number": 3, "englishName": "Ali 'Imran", "name": "آل عمران"},
-    {"number": 4, "englishName": "An-Nisa'", "name": "النساء"},
-    {"number": 5, "englishName": "Al-Ma'idah", "name": "المائدة"},
-  ];
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!mounted) return;
+
+      final surahVM = context.read<SurahViewModel>();
+
+      surahVM.fetchSurahList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = Provider.of<QuranPlayerViewModel>(context);
+    final reciterVM = context.watch<ReciterViewModel>();
+    final surahVM = context.watch<SurahViewModel>();
 
     return Scaffold(
-      appBar: AppBar(title: Text(viewModel.selectedReciter ?? "Reciter")),
-      body: ListView.builder(
-        itemCount: surahList.length,
-        itemBuilder: (context, index) {
-          final surah = surahList[index];
-          return ListTile(
-            leading: Text(
-              "${surah['number']}".padLeft(2, '0'),
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            title: Text(surah['englishName']),
-            subtitle: Text(
-              surah['name'],
-              textDirection: TextDirection.rtl,
-              style: const TextStyle(fontSize: 18),
-            ),
+      appBar: AppBar(
+        title: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            reciterVM.selectedReciter ?? "Reciter",
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      body: Builder(
+        builder: (context) {
+          if (surahVM.isLoading) {
+            return const Center(child: LoadingIndicator());
+          }
+
+          if (surahVM.surahList.isEmpty) {
+            return const Center(child: Text("Tidak ada data surah."));
+          }
+
+          return ListView.separated(
+            itemCount: surahVM.surahList.length,
+            separatorBuilder: (context, index) =>
+                const Divider(color: Colors.grey, thickness: 0.3, height: 1),
+            itemBuilder: (context, index) {
+              final SurahModel surah = surahVM.surahList[index];
+
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8,
+                  vertical: 15,
+                ),
+                child: ListTile(
+                  leading: Container(
+                    width: 60,
+                    height: 60,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/images/ornaments/img-frame-ayat.png',
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Text(
+                      surah.number.toString().padLeft(2, '0'),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: AppConstants.kFontSize,
+                      ),
+                    ),
+                  ),
+
+                  title: Text(
+                    surah.englishName,
+                    style: TextStyle(fontSize: AppConstants.kFontSize),
+                  ),
+
+                  trailing: Text(
+                    surah.name,
+                    textDirection: TextDirection.rtl,
+                    style: const TextStyle(fontSize: AppConstants.kFontSize),
+                  ),
+                  onTap: () {
+                    surahVM.setSelectedSurah(surah.number);
+                    Navigator.pushNamed(context, '/quranPlayer');
+                  },
+                ),
+              );
+            },
           );
         },
       ),
