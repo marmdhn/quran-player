bf082f14f82e37e4b2824e5d94868447aa4f0024
marmdhn <makbarramadhan62@gmail.com>
2025-11-08 01:43:19 +0700

    feat: add kFontSize constant to constants file

--- lib/core/config/constant.dart
+++ lib/core/config/constant.dart
@@ -2,6 +2,7 @@ class AppConstants {
   static const double kDefaultPadding = 16.0;
   static const double kDefaultRadius = 12.0;
   static const int kSplashDelay = 5;
+  static const double kFontSize = 14;
 
   static const String keyUserToken = "user_token";
   static const String keyOnboardingShown = "onboarding_shown";
