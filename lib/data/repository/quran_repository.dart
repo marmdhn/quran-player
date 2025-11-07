7d9d8db28c7859d54f581bfa5e4ecc1047fd3b5a
marmdhn <makbarramadhan62@gmail.com>
2025-11-08 01:42:49 +0700

    feat: add Surah and audio fetching methods to QuranRepository

--- lib/data/repository/quran_repository.dart
+++ lib/data/repository/quran_repository.dart
@@ -1,6 +1,8 @@
 import 'package:quran_player/core/services/api_instance.dart';
 
-import '../models/recreiter.dart';
+import '../models/ayah_model.dart';
+import '../models/recreiter_model.dart';
+import '../models/surah_model.dart';
 
 class QuranRepository {
   Future<List<ReciterModel>> getReciterList() async {
@@ -12,6 +14,33 @@ class QuranRepository {
       return data.map((e) => ReciterModel.fromMap(e)).toList();
     } catch (e) {
       throw Exception('Error fetching reciters: $e');
+    }
+  }
+
+  Future<List<SurahModel>> getSurah() async {
+    try {
+      final response = await apiAlQuran.get('surah');
+
+      final data = response['data'] as List;
+
+      return data.map((e) => SurahModel.fromMap(e)).toList();
+    } catch (e) {
+      throw Exception('Error fetching reciters: $e');
+    }
+  }
+
+  Future<SurahDetailModel> getSurahAudioList(
+    int surahId,
+    String reciterIdentifier,
+  ) async {
+    try {
+      final response = await apiAlQuran.get(
+        'surah/$surahId/$reciterIdentifier',
+      );
+
+      return SurahDetailModel.fromMap(response);
+    } catch (e) {
+      throw Exception('Error fetching surah audio list: $e');
     }
   }
 }
