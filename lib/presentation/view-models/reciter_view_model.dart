a44d0c9616bd8bc6248fe9129af9631b7291da2b
marmdhn <makbarramadhan62@gmail.com>
2025-11-08 01:41:52 +0700

    refactor: rename QuranPlayerViewModel to ReciterViewModel and enhance reciter selection logic

--- lib/presentation/view-models/reciter_view_model.dart
+++ lib/presentation/view-models/reciter_view_model.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 
 import '../../core/di/locator.dart';
-import '../../data/models/recreiter.dart';
+import '../../data/models/recreiter_model.dart';
 import '../../data/repository/quran_repository.dart';
 
-class QuranPlayerViewModel extends ChangeNotifier {
+class ReciterViewModel extends ChangeNotifier {
   final QuranRepository _repo = getIt<QuranRepository>();
+
   bool isLoading = false;
   List<ReciterModel> reciters = [];
-
   String? selectedReciter;
+  String? selectedReciterIdentifier;
 
   void setSelectedReciter(String reciter) {
     selectedReciter = reciter;
+    notifyListeners();
+  }
+
+  void setSelectedReciterIdentifier(String id) {
+    selectedReciterIdentifier = id;
     notifyListeners();
   }
 
@@ -23,7 +29,7 @@ class QuranPlayerViewModel extends ChangeNotifier {
     try {
       reciters = await _repo.getReciterList();
     } catch (e) {
-      debugPrint('Error: $e');
+      debugPrint('Error fetch reciters: $e');
     } finally {
       isLoading = false;
       notifyListeners();
