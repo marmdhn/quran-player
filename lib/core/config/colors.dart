82e975668ef2eaa0387e3018b63f6c759a1d31e6
marmdhn <makbarramadhan62@gmail.com>
2025-11-08 01:43:24 +0700

    feat: expand AppColors with additional color constants

--- lib/core/config/colors.dart
+++ lib/core/config/colors.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF4F5686);
-  static const Color lightDark = Color(0xFF9E9E9E);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color secondary = Color(0xFFA3A3A3);
+  static const Color lightDark = Color(0xFF404040);
+  static const Color dark = Color(0xFF2A2A2A);
   static const Color background = Color(0xFF1A1A1A);
 }
