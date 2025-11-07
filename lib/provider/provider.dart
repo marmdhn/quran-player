aab4956aec18eb21e432ef83ac1960878ec7aaf1
marmdhn <makbarramadhan62@gmail.com>
2025-11-08 01:41:34 +0700

    feat: add Reciter, Surah, and AudioPlayer view models to app providers

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -1,10 +1,14 @@
 import 'package:provider/provider.dart';
-import 'package:quran_player/presentation/view-models/quran_player_view_model.dart';
+import 'package:quran_player/presentation/view-models/audio_player_view_model.dart';
+import 'package:quran_player/presentation/view-models/reciter_view_model.dart';
+import 'package:quran_player/presentation/view-models/surah_view_model.dart';
 
 List<ChangeNotifierProvider> buildAppProviders() {
   return [
-    ChangeNotifierProvider<QuranPlayerViewModel>(
-      create: (_) => QuranPlayerViewModel(),
+    ChangeNotifierProvider<ReciterViewModel>(create: (_) => ReciterViewModel()),
+    ChangeNotifierProvider<SurahViewModel>(create: (_) => SurahViewModel()),
+    ChangeNotifierProvider<AudioPlayerViewModel>(
+      create: (_) => AudioPlayerViewModel(),
     ),
   ];
 }
