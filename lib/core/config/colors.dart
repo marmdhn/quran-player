a9b835f880e902ca370bd7d6ddc62ad32e117ed8
marmdhn <makbarramadhan62@gmail.com>
2025-11-07 15:19:56 +0700

    refactor: update background color and remove unused color constants in AppColors

--- lib/core/config/colors.dart
+++ lib/core/config/colors.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color(0xFF4F5686);
-  static const Color secondary = Color(0xFFF17641);
-  static const Color success = Color(0xFF05AA5B);
-  static const Color danger = Color(0xFFDA3732);
-  static const Color dark = Color(0xFF212529);
   static const Color lightDark = Color(0xFF9E9E9E);
-  static const Color background = Color(0xFFF0F2FF);
+  static const Color background = Color(0xFF1A1A1A);
 }
